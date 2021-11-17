eacba4c4ede8101bfb80a990cf9603b76e005d05
Holy Crusader <67459553+holy-tanuki@users.noreply.github.com>
2021-11-17 17:17:53 +0100

    Love button now work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,16 +7,16 @@
     </ion-header>
     <ion-content :fullscreen="true">  
       <ion-img class="logoTop" :src="logo.src" ></ion-img>
-      <div id="dating-card-container">
+      <div v-on:loved="sayHello" id="dating-card-container">
         <DatingCard class="datingCard" ref="datingCard" profileImage="./assets/example/date-profile.png" name="Andrass" age="63"></DatingCard>
-        <DatingCard class="datingCard" ref="datingCard" profileImage="./assets/example/date-profile3.png" name="1n3rn3t Gr4ndm4" age="83"></DatingCard>
+        <DatingCard class="datingCard" ref="datingCard" profileImage="./assets/example/date-profile3.png" name="1n3rn3t Gr4ndm4" age=""></DatingCard>
         <DatingCard class="datingCard" ref="datingCard" profileImage="./assets/example/date-profile2.png" name="Pappy-plier" age="59"></DatingCard>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { createGesture, createAnimation, IonPage, IonContent, IonImg } from '@ionic/vue';
 
 import DatingCard from '@/components/DatingCard.vue';
@@ -25,7 +25,7 @@ import DatingCard from '@/components/DatingCard.vue';
 export default {
   name: 'Home',
   components: { DatingCard, IonContent, IonPage, IonImg },
-  setup() {
+  data() {
     const logo = {
       'text': 'Logo',
       'src': './assets/icon/icon-transparent.png'
@@ -35,24 +35,28 @@ export default {
     }
   },
   methods: {
+    sayHello: () => {
+        console.log('Hello World 🍑')
+        console.log(this);
+    }
   },
   mounted: function mounted () {
     const MAX_TRANSLATE = 800;
 
 
     const getCardRef = () => {
-      let cardRef: any = null;
+      let cardRef = null;
       cardRef = document.querySelector(".datingCard")
       cardRef.classList.add("focusedCard")
       console.log(cardRef);
       return cardRef
     }
 
-    function initNewCard(elem: any) {
+    function initNewCard(elem) {
       if (!elem) {return}
       let started = false;
 
-      const getStep = (ev: any) => {
+      const getStep = (ev) => {
         // clamp
         return Math.max(0, Math.min(ev.deltaX / MAX_TRANSLATE, 1));
       }
@@ -63,7 +67,7 @@ export default {
         .fromTo('transform', 'translate(0, 0) rotate(0deg)', `translate(${MAX_TRANSLATE}px, -200px) rotate(25deg)`)
 
 
-      const onMove = (ev: any) => {
+      const onMove = (ev) => {
         if (!started) {
             cardAnimation.progressStart();
             started = true;
@@ -81,7 +85,7 @@ export default {
         onEnd: ev => onEnd(ev)
       });
 
-      const onEnd = (ev: any) => {
+      const onEnd = (ev) => {
           if (!started) { return; }
 
           cardGesture.enable(false);
@@ -103,6 +107,18 @@ export default {
 
           started = false;
       }
+
+      elem.querySelector('.btnLove').addEventListener('click', () => {
+        cardAnimation
+          .progressEnd(1, 0)
+          .onFinish(() =>{
+              cardGesture.enable(true);
+              if (elem) {
+                elem.remove();
+                initNewCard(getCardRef())
+              }
+          });
+      })
 
       cardGesture.enable(true);
     }
